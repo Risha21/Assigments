24adb0e1db2bd38d75fce68adf36281138d5c72f
Risha21 <70362309+Risha21@users.noreply.github.com>
2020-10-13 22:45:57 +0530

    Update Assigment3.java
    Below methods are updated :
    MinMaXIndexDifference
    evenOddNumbers

--- Naveen_Automation_CoreJava/src/ArrayConcept/Assigment3.java
+++ Naveen_Automation_CoreJava/src/ArrayConcept/Assigment3.java
@@ -1,115 +1,89 @@
 package ArrayConcept;
 
-import java.util.HashMap;
-
 public class Assigment3 {
 
-	public static int printIndexOfMaxNum(int numbers[]) {
-		int max = numbers[0], min = numbers[0];
-		int indexMax = 0;
-		for (int i = 0; i < numbers.length; i++) {
-			if (numbers[i] > max) {
-				max = numbers[i];
-				indexMax = i;
-			} else if (numbers[i] < min) {
-				min = numbers[i];
+	public static void MinMaXIndexDifference(int numbers[]) {
+		int min = numbers[0], max = numbers[0], minIndex = 0, maxIndex = 0;
 
-			}
-		}
-		return indexMax;
-
-	}
-
-	public static int printIndexOfMinNum(int numbers[]) {
-		int max = numbers[0], min = numbers[0];
-		int indexMin = 0;
-		for (int i = 0; i < numbers.length; i++) {
-			if (numbers[i] < min) {
-				min = numbers[i];
-				indexMin = i;
-			} else if (numbers[i] > max) {
-				max = numbers[i];
-
-			}
-		}
-		return indexMin;
-
-	}
-
-	public static int differenceMaxMinNumber(int numbers[]) {
-		int max = numbers[0], min = numbers[0];
-		int difference = 0;
 		for (int i = 1; i < numbers.length; i++) {
 			if (numbers[i] > max) {
 				max = numbers[i];
-				System.out.println(i);
-			} else if (numbers[i] < min) {
+				maxIndex = i;
+			} else {
 				min = numbers[i];
+				minIndex = i;
 			}
-
-			difference = max - min;
-
 		}
-		return difference;
 
+		System.out.println("Index of element holding maximum value is:" + maxIndex);
+		System.out.println("Index of element holding minimum value is:" + minIndex);
+		System.out.println(max - min);
 	}
 
 	public static void evenOddNumbers(int numbers[]) {
+		System.out.print("[");
 		for (int i = 0; i < numbers.length; i++) {
 			if (numbers[i] % 2 == 0) {
-				System.out.print(" even ");
+				System.out.print("\"");
+				System.out.print("even");
 
 			} else {
-				System.out.print(" odd ");
+				System.out.print("\"");
+				System.out.print("odd");
+
 			}
+			System.out.print("\"");
+			System.out.print(",");
 
 		}
+		System.out.print("]");
 
 	}
 
 	public static void main(String[] args) {
 
 		int arr[] = new int[] { 34, 8, 10, 3, 2, 80, 30, 33, 1 };
-		System.out.println("Index of an integer holding maximum value is:"+printIndexOfMaxNum(arr));
-		System.out.println("Index of an integer holding maximum value is:"+printIndexOfMinNum(arr));
-		System.out.println("Difference between maximum and minimum value in an array is:" + differenceMaxMinNumber(arr));
+		MinMaXIndexDifference(arr);
 		evenOddNumbers(arr);
-		
-
 	}
 
 }
+
 /*
- * System.out.println("number of elements available in array are:"+arr.length);
- * /*for(int i=0;i<arr.length;i++) { if(arr[i]%2==0) {
- * System.out.print(" even "); } else { System.out.print(" odd "); }
- */
-/*
- * int min=arr[0]; System.out.println(min); for(int i=1;i<arr.length;i++) {
- * if(arr[i]<min) { min=arr[i]; System.out.println(min); } } //
- * System.out.print(arr[i]); // HashMap<Integer,Integer> hm=new
- * HashMap<Integer,Integer>;
- * 
- * 
- * }
- */
-/*
- * public static int maxInteger(int numbers[]) { int max=numbers[0]; for(int
- * i=1;i<numbers.length;i++) { if(numbers[i]>max) { max=numbers[i]; } }
- * 
- * return max; }
- * 
- * public static int minInt(int numbers[]) { int min=numbers[0]; for(int
- * i=1;i<numbers.length;i++) { if(numbers[i]<min) { min=numbers[i]; } } return
- * min;
+ * public static int printIndexOfMaxNum(int numbers[]) { int max = numbers[0],
+ * min = numbers[0]; int indexMax = 0; for (int i = 0; i < numbers.length; i++)
+ * { if (numbers[i] > max) { max = numbers[i]; indexMax = i; } } return
+ * indexMax;
  * 
  * }
  * 
- * /*public void differenceMaxMin() {
+ * public static int printIndexOfMinNum(int numbers[]) { int max = numbers[0],
+ * min = numbers[0]; int indexMin = 0; for (int i = 0; i < numbers.length; i++)
+ * { if (numbers[i] < min) { min = numbers[i]; indexMin = i; } else if
+ * (numbers[i] > max) { max = numbers[i];
  * 
- * System.out.println(max); System.out.println(min); // int difference=max-min;
- * // return difference; return max;
+ * } } return indexMin; }
  * 
+ * 
+ * public static int differenceMaxMinNumber(int numbers[]) { int max =
+ * numbers[0], min = numbers[0]; for (int i = 1; i < numbers.length; i++) { if
+ * (numbers[i] > max) { max = numbers[i]; } else if (numbers[i] < min) { min =
+ * numbers[i]; }
+ * 
+ * } return max - min;
  * 
  * }
+ * 
+ * public static void evenOddNumbers(int numbers[]) {
+ * 
+ * for (int i = 0; i < numbers.length; i++) { if (numbers[i] % 2 == 0) {
+ * System.out.print(" even ");
+ * 
+ * } else { System.out.print(" odd "); }
+ * 
+ * }
+ * 
+ * }
+ * 
+ * 
  */
